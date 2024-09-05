d624c2b8818233455cefdab282fdd04650dd1581
sam <samhutchins123@gmail.com>
2024-09-04 21:08:05 -0700

    built boilerplate
    added the base for the restful api service, including unit testing

---
+++ webapp.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    return jsonify([{'id': item.id, 'name': item.name} for item in items])
+
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.json
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({'id': new_item.id, 'name': new_item.name}), 201
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
