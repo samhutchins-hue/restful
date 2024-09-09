f11a452c5060953692a3bcf2c6f1001373405bbc
Samuel Hutchins <samhutchins123@gmail.com>
2024-09-08 22:39:30 -0700

    changed character limit for lincoln

--- restful-backend/myapp/models.py
+++ restful-backend/myapp/models.py
@@ -3,4 +3,4 @@ from .extensions import db
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(), nullable=False)
